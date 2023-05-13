e3da5aad624540aea52c28d49c8f49d399e8806a
Arwafawzi <130005672+Arwafawzi@users.noreply.github.com>
2023-05-13 14:22:07 +0300

    Add files via upload

---
+++ proj1/bin/if.dart
@@ -0,0 +1,21 @@
+ void main() {
+  //--------if -----nora ---is older than sahar-------------------------
+  int sahar=20;
+int nora =34;
+if(nora >sahar){print("yes nora is older than sahar");}
+else{print("no");}
+//--------------------------
+ var x=7;
+ if(x > 10){print ("the number is greater than 10");}
+ else if(x <=0) {print("The number is less than or equal to 10'");}
+else  {print(' The number is negative');}
+//---------------------------------------------------
+String hello="hello";
+if(hello=="hello")
+{print("The string is hello");}
+else if(hello ==null){print("The string is null");} 
+else  {print("string is not hello");}
+//---------------------------------------------------
+
+
+}
